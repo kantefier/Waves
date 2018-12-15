df40fe19dc6f59d2b9c30bee82189e84aa491084
Ruslan Kalimullin <kalimullin.ruslan@gmail.com>
2018-12-15 15:53:22 +0300

    Update sql scripts

--- src/main/resources/schema/schema.sql
+++ src/main/resources/schema/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE public.blocks
   fee                                bigint                      NOT NULL,
   blocksize                          integer,
   height                             integer                     NOT NULL PRIMARY KEY,
-  features                           smallint[]
+  features                           smallint[],
+  block_bytes                        bytea                       NOT NULL
 );
 
 -- common table for all transactions
@@ -252,4 +253,171 @@ CREATE TABLE public.set_asset_script_transactions
   FOREIGN KEY ("height") REFERENCES "public"."blocks" ("height")
 )
   INHERITS (public.transactions);
+
+-- addresses
+CREATE TABLE public.addresses
+(
+  id      BIGINT            NOT NULL PRIMARY KEY,
+  address character varying NOT NULL
+);
+
+-- Waves balance history
+CREATE TABLE public.waves_balance_history
+(
+  address_id BIGINT    NOT NULL REFERENCES public.addresses ("id"),
+  heights    integer[] NOT NULL,
+  PRIMARY KEY (address_id)
+);
+
+-- current Waves balance
+CREATE TABLE public.current_waves_balance
+(
+  address_id BIGINT  NOT NULL REFERENCES public.addresses ("id"),
+  height     integer NOT NULL REFERENCES "public"."blocks" ("height"),
+  amount     BIGINT  NOT NULL,
+  PRIMARY KEY (address_id, height)
+);
+
+-- Assets for address
+CREATE TABLE public.addresses_assets
+(
+  address_id BIGINT              NOT NULL REFERENCES public.addresses ("id"),
+  assets     character varying[] NOT NULL,
+  PRIMARY KEY (address_id)
+);
+
+-- Assets balance history
+CREATE TABLE public.assets_balance_history
+(
+  address_id BIGINT            NOT NULL REFERENCES public.addresses ("id"),
+  asset_id   character varying NOT NULL,
+  heights    integer[]         NOT NULL
+);
+
+-- current Asset balance
+CREATE TABLE public.current_asset_balance
+(
+  address_id BIGINT            NOT NULL REFERENCES public.addresses ("id"),
+  asset_id   character varying NOT NULL,
+  amount     BIGINT,
+  PRIMARY KEY (address_id, asset_id)
+);
+
+-- Assets info history
+CREATE TABLE public.assets_info_history
+(
+  asset_id character varying NOT NULL,
+  heights  integer[]         NOT NULL,
+  PRIMARY KEY (asset_id)
+);
+
+-- AssetInfo
+CREATE TABLE public.assets_info
+(
+  asset_id      character varying NOT NULL,
+  is_reissuable boolean           NOT NULL default false,
+  volume        bigint            not null,
+  height        integer           NOT NULL REFERENCES "public"."blocks" ("height"),
+  PRIMARY KEY (asset_id, height)
+);
+
+-- Lease balance history
+CREATE TABLE public.lease_balance_history
+(
+  address_id BIGINT    NOT NULL REFERENCES public.addresses ("id"),
+  heights    integer[] NOT NULL,
+  PRIMARY KEY (address_id)
+);
+
+-- Lease balance for address at height
+CREATE TABLE public.lease_balance_at_height
+(
+  address_id BIGINT  NOT NULL REFERENCES public.addresses ("id"),
+  height     integer NOT NULL REFERENCES "public"."blocks" ("height"),
+  "in"       BIGINT  NOT NULL,
+  out        BIGINT  NOT NULL,
+  PRIMARY KEY (address_id, height)
+);
+
+-- Lease status history
+CREATE TABLE public.lease_status_history
+(
+  lease_id CHARACTER VARYING NOT NULL REFERENCES public.lease_transactions ("id"),
+  heights  integer[]         NOT NULL,
+  PRIMARY KEY (lease_id)
+);
+
+-- Lease status at height
+CREATE TABLE public.lease_status_at_height
+(
+  lease_id CHARACTER VARYING NOT NULL REFERENCES public.lease_transactions ("id"),
+  height   integer           NOT NULL REFERENCES "public"."blocks" ("height"),
+  status   BOOLEAN           NOT NULL,
+  PRIMARY KEY (lease_id, height)
+);
+
+-- Fill volume and fill history
+CREATE TABLE public.filled_volume_and_fee_history
+(
+  order_id CHARACTER VARYING NOT NULL,
+  heights  INTEGER[]         NOT NULL,
+  PRIMARY KEY (order_id)
+);
+
+-- Volume and fee for order at height
+CREATE TABLE public.volume_and_fee_for_order_at_height
+(
+  order_id CHARACTER VARYING NOT NULL,
+  height   integer           NOT NULL REFERENCES "public"."blocks" ("height"),
+  volume   BIGINT            NOT NULL,
+  fee      BIGINT            NOT NULL,
+  PRIMARY KEY (order_id, height)
+);
+
+-- Changed addresses at height
+CREATE TABLE public.changed_addresses_at_height
+(
+  height        integer  NOT NULL REFERENCES "public"."blocks" ("height"),
+  addresses_ids BIGINT[] NOT NULL, -- TODO: better use references to addresses table
+  PRIMARY KEY (height)
+);
+
+-- aliases
+CREATE TABLE public.aliases
+(
+  alias      character varying NOT NULL PRIMARY KEY,
+  address_id BIGINT            NOT NULL REFERENCES public.addresses ("id")
+);
+
+-- Script for addressId at height
+CREATE TABLE public.address_scripts_at_height
+(
+  address_id BIGINT  NOT NULL REFERENCES public.addresses ("id"),
+  height     integer NOT NULL REFERENCES "public"."blocks" ("height"),
+  script     bytea,
+  PRIMARY KEY (address_id, height)
+);
+
+-- Approved features
+CREATE TABLE public.approved_features
+(
+  id     INTEGER NOT NULL PRIMARY KEY,
+  height integer NOT NULL REFERENCES "public"."blocks" ("height")
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
